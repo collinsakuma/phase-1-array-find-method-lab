7241b3312327f4ee3003fbbc12f045a581d79f69
Collin Sakuma <collinmsakuma@gmail.com>
2023-02-02 08:50:31 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+const record = [
+    { 
+        year: "2018",
+        result: "N/A"
+    },
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+
+]
+
+
+function callBack(element) {
+    if(element.result === 'W') {
+        return element;
+    }
+}
+
+function superbowlWin (array) {
+    let answer = array.find(callBack);
+    if (typeof answer === 'object') {
+        return answer.year;
+    }
+} 
